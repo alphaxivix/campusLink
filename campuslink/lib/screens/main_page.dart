fe4e411c54db7bf1b1003e56f3150491d4470846
aswin6373 <aswinarun637@example.com>
2024-12-21 10:56:29 +0530

    change the style of bottom navigation bar

--- campuslink/lib/screens/main_page.dart
+++ campuslink/lib/screens/main_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:campuslink/screens/admin_dashboard.dart/admin_dashboard.dart';
 import 'package:campuslink/screens/chatbot/chatbot.dart';
 import 'package:campuslink/screens/chatroom/chatroom.dart';
@@ -8,121 +7,94 @@ import 'package:campuslink/screens/student_dashboard/student_dashboard.dart';
 import 'package:campuslink/screens/teacher_dashboard/teacher_dashboard.dart';
 import 'package:flutter/material.dart';
 
-class MainPage extends StatefulWidget {
-  final String userType; // Pass the user type as a parameter (e.g., "Guest", "Admin")
+class NavItem {
+  final IconData icon;
+  final String label;
+  final Color activeColor;
 
-  MainPage({required this.userType});
+  const NavItem({
+    required this.icon,
+    required this.label,
+    this.activeColor = const Color(0xFF6200EE),
+  });
+}
+
+class MainPage extends StatefulWidget {
+  final String userType;
+
+  const MainPage({super.key, required this.userType});
 
   @override
-  _MainPageState createState() => _MainPageState();
+  State<MainPage> createState() => _MainPageState();
 }
 
 class _MainPageState extends State<MainPage> {
   int _currentIndex = 0;
-
   late List<Widget> _pages;
-  late List<BottomNavigationBarItem> _bottomNavItems;
+  late List<NavItem> _navItems;
 
   @override
   void initState() {
     super.initState();
+    _initializeNavigation();
+  }
 
-    // Define pages and navigation items based on userType
-    if (widget.userType == 'Guest') {
-      _pages = [
-        GuestDashboard(),
-        CommunityPost(),
-        Chatbot(),
-      ];
-      _bottomNavItems = [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.dashboard_rounded),
-          label: 'Dashboard',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.group_rounded),
-          label: 'Community',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.smart_toy),
-          label: 'Chatbot',
-        ),
-      ];
-    } else if (widget.userType == 'Admin') {
-      _pages = [
-        AdminDashboard(),
-        CommunityPost(),
-        Chatroom(),
-        Chatbot(),
-      ];
-      _bottomNavItems = [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.dashboard_rounded),
-          label: 'Dashboard',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.group_rounded),
-          label: 'Community',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.chat_rounded),
-          label: 'Chatroom',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.smart_toy),
-          label: 'Chatbot',
-        ),
-      ];
-    } else if (widget.userType == 'Student') {
-      _pages = [
-        StudentDashboard(), // Update with StudentDashboard
-        CommunityPost(),
-        Chatroom(),
-        Chatbot(),
-      ];
-      _bottomNavItems = [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.dashboard_rounded),
-          label: 'Dashboard',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.group_rounded),
-          label: 'Community',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.chat_rounded),
-          label: 'Chatroom',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.smart_toy),
-          label: 'Chatbot',
-        ),
-      ];
-    } else if (widget.userType == 'Teacher') {
-      _pages = [
-        TeacherDashboard(), // Update with TeacherDashboard
-        CommunityPost(),
-        Chatroom(),
-        Chatbot(),
-      ];
-      _bottomNavItems = [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.dashboard_rounded),
-          label: 'Dashboard',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.group_rounded),
-          label: 'Community',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.chat_rounded),
-          label: 'Chatroom',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.smart_toy),
-          label: 'Chatbot',
-        ),
-      ];
+  void _initializeNavigation() {
+    final baseNavItems = <NavItem>[
+      const NavItem(
+        icon: Icons.dashboard_rounded,
+        label: 'Dashboard',
+        activeColor: Color(0xFF6200EE),
+      ),
+      const NavItem(
+        icon: Icons.group_rounded,
+        label: 'Community',
+        activeColor: Color(0xFF03DAC6),
+      ),
+    ];
+
+    switch (widget.userType) {
+      case 'Guest':
+        _pages = [
+          GuestDashboard(),
+          CommunityPost(),
+          Chatbot(),
+        ];
+        _navItems = [
+          ...baseNavItems,
+          const NavItem(
+            icon: Icons.smart_toy,
+            label: 'Chatbot',
+            activeColor: Color(0xFFBB86FC),
+          ),
+        ];
+      case 'Admin':
+      case 'Student':
+      case 'Teacher':
+        _pages = [
+          if (widget.userType == 'Admin')
+            AdminDashboard()
+          else if (widget.userType == 'Student')
+            StudentDashboard()
+          else
+            TeacherDashboard(),
+          CommunityPost(),
+          Chatroom(),
+          Chatbot(),
+        ];
+        _navItems = [
+          ...baseNavItems,
+          const NavItem(
+            icon: Icons.chat_rounded,
+            label: 'Chatroom',
+            activeColor: Color(0xFF018786),
+          ),
+          const NavItem(
+            icon: Icons.smart_toy,
+            label: 'Chatbot',
+            activeColor: Color(0xFFBB86FC),
+          ),
+        ];
     }
   }
 
@@ -130,26 +102,66 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _pages[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: const Color.fromARGB(255, 41, 51, 56),
-        selectedItemColor: const Color.fromARGB(255, 121, 130, 139),
-        unselectedItemColor: Colors.white,
-        currentIndex: _currentIndex,
-        elevation: 12,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: _bottomNavItems,
-        selectedLabelStyle: TextStyle(
-          fontWeight: FontWeight.bold,
-          color: Colors.blue[700],
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 12,
+              offset: const Offset(0, -2),
+            ),
+          ],
         ),
-        unselectedLabelStyle: TextStyle(
-          fontWeight: FontWeight.normal,
-          color: Colors.grey[600],
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: List.generate(
+            _navItems.length,
+            (index) => _buildNavItem(_navItems[index], index),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNavItem(NavItem item, int index) {
+    final isSelected = _currentIndex == index;
+    
+    return GestureDetector(
+      onTap: () => setState(() => _currentIndex = index),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        padding: EdgeInsets.symmetric(
+          horizontal: isSelected ? 16.0 : 12.0,
+          vertical: 8.0,
+        ),
+        decoration: BoxDecoration(
+          color: isSelected 
+              ? item.activeColor.withOpacity(0.1)
+              : Colors.transparent,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              item.icon,
+              color: isSelected ? item.activeColor : Colors.grey[600],
+              size: isSelected ? 28 : 24,
+            ),
+            if (isSelected) ...[
+              const SizedBox(width: 8),
+              Text(
+                item.label,
+                style: TextStyle(
+                  color: item.activeColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                ),
+              ),
+            ],
+          ],
         ),
       ),
     );
